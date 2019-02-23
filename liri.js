8db29d07c0c5d0843adf4a0fee064ce61cf336b8
Paige Herbst <PMoney@Paiges-MacBook-Pro.local>
2019-02-23 12:07:00 -0700

    last change

---
+++ liri.js
@@ -0,0 +1,139 @@
+require("dotenv").config();
+
+var Spotify = require("node-spotify-api");
+var keys = require("./keys");
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
+var spotify = new Spotify(keys.spotify);
+
+var artistNames = function(artist) {
+    return artist.name;
+  };
+  
+  var goToSpotify = function(songName) {
+    if (songName === undefined) {
+      songName = "god is a woman";
+    }
+  
+    spotify.search(
+      {
+        type: "track",
+        query: songName
+      },
+      function(err, data) {
+        if (err) {
+          console.log("Error occurred: " + err);
+          return;
+        }
+  
+        var songs = data.tracks.items;
+  
+        for (var i = 0; i < songs.length; i++) {
+          console.log(i);
+          console.log("artist(s): " + songs[i].artists.map(artistNames));
+          console.log("song name: " + songs[i].name);
+          console.log("preview song: " + songs[i].preview_url);
+          console.log("album: " + songs[i].album.name);
+          console.log("-----------------------------------");
+        }
+      }
+    );
+  };
+  
+  var showBands = function(artist) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+  
+    axios.get(queryURL).then(
+      function(response) {
+        var jsonData = response.data;
+  
+        if (!jsonData.length) {
+          console.log("We can't find this artist:  " + artist);
+          return;
+        }
+  
+        console.log("Concerts coming up for " + artist + ":");
+  
+        for (var i = 0; i < jsonData.length; i++) {
+          var show = jsonData[i];
+          console.log(
+            show.venue.city +
+              "," +
+              (show.venue.region || show.venue.country) +
+              " at " +
+              show.venue.name +
+              " " +
+              moment(show.datetime).format("MM/DD/YYYY")
+          );
+        }
+      }
+    );
+  };
+  
+  var findMovie = function(movieName) {
+    if (movieName === undefined) {
+      movieName = "Mr Nobody";
+    }
+  
+    var urlHit =
+      "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+        
+    axios.get(urlHit).then(
+      function(response) {
+          console.log(response);
+        var jsonData = response.data;
+  
+        console.log("Title: " + jsonData.Title);
+        console.log("Year: " + jsonData.Year);
+        console.log("Rated: " + jsonData.Rated);
+        console.log("IMDB Rating: " + jsonData.imdbRating);
+        console.log("Country: " + jsonData.Country);
+        console.log("Language: " + jsonData.Language);
+        console.log("Plot: " + jsonData.Plot);
+        console.log("Actors: " + jsonData.Actors);
+        console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
+      }
+    );
+  };
+  
+  var showData = function() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+      console.log(data);
+  
+      var dataArr = data.split(",");
+  
+      if (dataArr.length === 2) {
+        pick(dataArr[0], dataArr[1]);
+      } else if (dataArr.length === 1) {
+        pick(dataArr[0]);
+      }
+    });
+  };
+  
+  var pick = function(caseData, functionData) {
+    switch (caseData) {
+    case "concert-this":
+      showBands(functionData);
+      break;
+    case "spotify-this-song":
+      goToSpotify(functionData);
+      break;
+    case "movie-this":
+      findMovie(functionData);
+      break;
+    case "do-what-it-says":
+      showData();
+      break;
+    default:
+      console.log("LIRI can't find results");
+    }
+  };
+  
+  
+  var runIt = function(argOne, argTwo) {
+    pick(argOne, argTwo);
+  };
+  
+  runIt(process.argv[2], process.argv.slice(3).join(" "));
+  
